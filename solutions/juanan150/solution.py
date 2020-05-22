a04f378cd88690da923d878cb9f2e8616d07818a
Juan Andres Quiroz <juanan0309@gmail.com>
2020-05-22 09:53:38 -0500

    Ahora si agrego solucion

---
+++ solutions/juanan150/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    """Objeto del ejercicio"""
+    def duplicate_zeros(self, arr: List[int]):
+        """Funcion que duplica los 0s"""
+        temp = list(arr)
+        j = 0
+        for val in temp:
+            if j >= len(temp)-1:
+                break
+
+            if val == 0:
+                j += 1
+                arr.insert(j, 0)
+                arr.pop(-1)
+
+            j += 1
